99b54c55cf7de898246063afefc7ed21d10edd3e
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-05-02 18:44:25 +0200

    complete snack 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import useSwitch from "./customHooks/useSwitch";
 
 function App() {
+  const [isOn, toggle] = useSwitch();
 
   return (
     <>
-      <h1>Hello World!</h1>
+      {/* Snack 1 */}
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
     </>
   )
 }
